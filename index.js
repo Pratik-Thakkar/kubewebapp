a44467bb3b2b06418f9ab9e4ad0e08be263d66c3
Pratik Thakkar <thakkarpratik277@gmail.com>
2022-05-27 19:00:33 +0530

    Updated message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ async function main() {
     const app      = express();
     const port     = 3000;
     
-    app.get('/', (req, res) => res.json({hello: 'world'}));
+    app.get('/', (req, res) => res.json({hello: 'world from Pratik'}));
     
     await app.listen(port);
     console.log(`Listening on port ${port}.`);
