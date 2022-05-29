c930f11db0490523d3042f4c8360c2f613f8c760
Pratik Thakkar <thakkarpratik277@gmail.com>
2022-05-29 13:32:15 +0530

    Added logic for terraform replace

--- .github/workflows/tf_apply.sh
+++ .github/workflows/tf_apply.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-chart = $(terraform state list helm_release.webapp)
+CHART=$(terraform state list helm_release.webapp)
 
-if [[ $chart = 'helm_release.webapp']]
+if [[ $CHART == 'helm_release.webapp' ]]
 then
-    terraform apply -replace="helm_release.webapp" -auto-approve -input=false
+ TF="terraform apply -replace="helm_release.webapp" -auto-approve -input=false"
+ echo $TF
 else
-    terraform apply -auto-approve -input=false
+ TF="terraform apply -auto-approve -input=false"
+ echo $TF
 fi
